4fbd310a1e3887b8077d7d1e5131f22aec526246
tomi-0 <asadetomi@gmail.com>
2025-02-19 00:05:23 +0000

    added functionality to all functions

--- hangman.py
+++ hangman.py
@@ -2,81 +2,122 @@
 
 import random
 
-words = ("maths", "english", "history", "art", "geography", "spanish" )
+words = ("maths", "english", "history", "art", "geography", "spanish")
 
 # dictionary of hangman art from @chrishorton
 hangman_art = {
     0: (
-        """
-        +---+
-        |   |
-            |
-            |
-            |
-            |
-        ========="""
+    """
+    +---+
+    |   |
+        |
+        |
+        |
+        |
+    ========="""
     ),
     1: (
-        """
-        +---+
-        |   |
-        O   |
-            |
-            |
-            |
-        ========="""
+    """
+    +---+
+    |   |
+    O   |
+        |
+        |
+        |
+    ========="""
     ),
     2: (
-        """
-        +---+
-        |   |
-        O   |
-        |   |
-            |
-            |
-        ========="""
+    """
+    +---+
+    |   |
+    O   |
+    |   |
+        |
+        |
+    ========="""
     ),
     3: (
-        """
-        +---+
-        |   |
-        O   |
-       /|   |
-            |
-            |
-        ========="""
+    """
+    +---+
+    |   |
+    O   |
+   /|   |
+        |
+        |
+    ========="""
     ),
+    # r - raw string w/ no escape sequences
     4: (
-        r"""
-        +---+
-        |   |
-        O   |
-       /|\  |
-            |
-            |
-        ========="""
+    r"""
+    +---+
+    |   |
+    O   |
+   /|\  |
+        |
+        |
+    ========="""
     ),
     5: (
-        r"""
-        +---+
-        |   |
-        O   |
-       /|\  |
-       /    |
-            |
-        ========="""
+    r"""
+    +---+
+    |   |
+    O   |
+   /|\  |
+   /    |
+        |
+    ========="""
     ),
     6: (
-        r"""
-        +---+
-        |   |
-        O   |
-       /|\  |
-       / \  |
-            |
-        ========="""
+    r"""
+    +---+
+    |   |
+    O   |
+   /|\  |
+   / \  |
+        |
+    ========="""
     ),
 }
 
-print(hangman_art.get(0))
+def display_man(wrong_guesses):
+    print(hangman_art.get(wrong_guesses))
+
+def display_hint(hint):
+    for char in hint:
+        print(char, end=" ")
+    print() 
+    print()
+
+def display_answer():
+    pass
+
+def main():
+    answer = random.choice(words)
+    hint = "_" * len(answer)
+    wrong_guesses = 0
+    game_over = False
+    
+    print("Welcome to Python Hangman")
+
+    while not game_over:
+        
+        display_man(wrong_guesses)
+        display_hint(hint)
+
+        guess = input("Enter a letter guess: ").lower()
+        if guess in answer:
+            for char in answer:
+                if char == guess:
+                    print()
+        else:
+            wrong_guesses += 1
+            if wrong_guesses == 6:
+                display_answer()
+                break
+
+    print("Game over!")
+
+if __name__ == "__main__":
+    main()
+
 
